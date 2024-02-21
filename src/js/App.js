699aa5f844fe3bfb43cae3a93d532a2fd1d97971
Jeffrey Martinez <martinezjf2@gmail.com>
2024-02-20 20:48:32 -0500

    learning basic tailwind

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <h1 className="text-5xl">My Name is Jeffrey</h1>
+      <h1 className="font-serif text-9xl font-black leading-loose">Title</h1>
+      <h2 className="fon-sans text-7xl text-base leading-relaxed underline capitalize italic text-center text-rose-200">
+        Subtitle
+      </h2>
+      <p className="">paragraph</p>
+      <div className="">box</div>
     </div>
   );
 }
